54d2e9f38e229f39282f769a4d422bca8ee8b4fc
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-19 18:19:26 +0500

    Updated menu

--- source/main.c
+++ source/main.c
@@ -10,7 +10,11 @@ void menu()
         printf("\n==========Supermarket Management System==========\n");
         printf("1.Add Items\n");
         printf("2.Display Stock\n");
-        printf("3.Exit\n");
+        printf("3.Update Item\n");
+        printf("4.Delete Item\n");
+        printf("5.Check For Low Stock\n");
+        printf("6.Exit\n");
+
         printf("Ener Your Choice: ");
         scanf("%d",&choice);
 
@@ -25,6 +29,18 @@ void menu()
                 break;
 
             case 3:
+                updateItem();
+                break;
+
+            case 4:
+                deleteItem();
+                break;
+
+            case 5:
+                lowStock();
+                break;
+
+            case 6:
                 printf("Exiting The System...\n");
                 break;
 
