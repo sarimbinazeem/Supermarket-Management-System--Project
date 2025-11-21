8991f6028c244b2ea346868da8ece2f847d0c00b
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-21 19:00:38 +0500

    Added Time Library

--- source/stock.c
+++ source/stock.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h> //For Timestamps
 #include "../include/stock.h"
 
 Product *inventory = NULL; //It starts from pointing to nothing
