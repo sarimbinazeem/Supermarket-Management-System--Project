e79ef43beeb370bf2cec434089ba328de6c26ca7
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-22 16:09:29 +0500

    Added Comments In Stock Header Files

--- include/stock.h
+++ include/stock.h
@@ -1,10 +1,13 @@
-//Header File For Stock
-
+//Header Files
 #ifndef STOCK_H
 #define STOCK_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-//Structure Added For Each Product
+//----------Products Structure----------
 typedef struct
 {
     int id;
@@ -13,7 +16,18 @@ typedef struct
     int quantity;
 }Product;
 
+//----------Sales Structure----------
+typedef struct
+{
+    int saleID;
+    int productID;
+    int quantitySold;
+    float totalPrice;
+    char productName[50];
+    char date[30]; //Added Date Array For Timestamps
+}Sale;
 
+//----------Stock Variables Structure----------
 typedef struct
 {
     Product *inventory;
@@ -21,6 +35,7 @@ typedef struct
     int capacity;
 } StockVariables;
 
+//----------Sales Variable Structure----------
 typedef struct
 {
     Sale * sales;
@@ -29,37 +44,35 @@ typedef struct
 } SaleVariables;
 
 
+//---------- Function Prototypes----------
 
-typedef struct
-{
-    int saleID;
-    int productID;
-    int quantitySold;
-    float totalPrice;
-    char date[30]; //Added Date Array For Timestamps
-}Sale;
-
-//Function Prototypes
-void capacityUpdater(StockVariables*s)
+//Stock Functions
+void loadStock(StockVariables *s);
+void saveStock(StockVariables *s);
 void addItem(StockVariables *s);
 void displayStock(StockVariables *s);
-void saveStock(StockVariables *s);
-void loadStock(StockVariables *s);
+void recursiveDisplay (StockVariables*s ,int index);
+void pointerPrice(Product *p, float newPrice);
 void updateItem(StockVariables *s);
 void deleteItem(StockVariables *s);
 void lowStock(StockVariables *s);
 void searchItems(StockVariables *s);
 void sortByName(StockVariables *s);
 void sortByPrice(StockVariables *s);
-void pointerPrice(Product *p, float newPrice);
-void recursiveDisplay (StockVariables*s ,int index);
-void recordSale(Product inventory[], int *itemCount, Sale sales[], int*saleCount);
-void salesReport();
 void cleanSystem(StockVariables *s, SaleVariables *sales);
 int getInteger();
 float getFloating();
+void capacityUpdater(StockVariables*s);
+
+//Sales Functions
+void recordSale(StockVariables *s, SalesVariables *sales);
+void salesReport(StockVariables *s, SalesVariables *sales);
 void saveSales(Sale sales[], int saleCount);
 void loadSales(Sale sales[], int *saleCount);
 
+//Menu Functions
+void menu(StockVariables *s, SalesVariables *sales);
+void adminMenu(StockVariables *s, SalesVariables *sales);
+void customerMenu(StockVariables *s, SalesVariables *sales);
 
 #endif
