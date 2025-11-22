63003b1a19acff028afdc984f4756563fa49bce9
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-22 20:37:36 +0500

    Added Comments TO iNPUT.c

--- source/input.c
+++ source/input.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "input.h"
 
+//--------Input Validator For Integers----------
 int getInteger(char *msg ,int min, int max)
 {
     int n;
     while(1)
     {
+        //Print Initial Message
         printf("%s",msg);
+        //Check IF THE number is Integer and In RANGE
         if((scanf("%d",&n))!= 1 && n>=min && n<=max)
         {
             return n;
@@ -18,12 +21,15 @@ int getInteger(char *msg ,int min, int max)
 
 }
 
+//----------Input Validator For Floating Numbers----------
 float getFloating(char *msg ,int min, int max)
 {
     float n;
     while(1)
     {
+        //Prints Initial Message
         printf("%s",msg);
+        //Checks If The Number Is In Range And A Float Number
         if((scanf("%f",&n))!= 1 && n>=min && n<=max)
         {
             return n;
