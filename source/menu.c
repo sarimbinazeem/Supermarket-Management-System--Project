7a1f08520947f09997af991308cb8c8e90106ee9
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-22 20:34:38 +0500

    Added Comments To Menu

--- source/menu.c
+++ source/menu.c
@@ -10,6 +10,7 @@ void adminMenu(StockVariables *stock, SaleVariables *sales)
 
     do
     {
+        //Menu Displayed For Admin
         printf("\n=====Admin Menu=====\n");
         printf("1.Add Items\n");
         printf("2.Display Stock\n");
@@ -24,52 +25,54 @@ void adminMenu(StockVariables *stock, SaleVariables *sales)
 
         choice = getInt("Enter choice: ", 0, 9);
 
-
+        //Switch cases for each cases
         switch(choice)
-        
-            case 1:
+        {
+            case 1: //Add New Items
                 addItem(stock);
                 break;
             
-            case 2:
+            case 2: //Display ALL Stocks
                 displayStock(stock);
                 break;
 
-            case 3:
+            case 3: //update an items price and quantity
                 updateItem(stock);
                 break;
 
-            case 4:
+            case 4: //Delete an Item FROm Stock
                 deleteItem(stock);
                 break;
 
-            case 5:
+            case 5: //Check If Any Item Have Low Stock
                 lowStock(stock);
                 break;
             
-            case 6:
+            case 6: //Search For An Item To Get Its Details
                 searchItems(stock);
                 break;
 
-            case 7:
+            case 7: //Sort The Stock By Name
                 sortByName(stock);
                 break;
 
-            case 8:
+            case 8: //Sort The Stock By Price
                 sortByPrice(stock);
                 break;
 
-            case 9:
+            case 9: //Get Report Of The Sales Done
                 salesReport(stock,sales);
                 break;
 
-             case 0:
+             case 0: //Exiting THe Program
                 printf("Logging Out...\n");
                 break;
 
-            default:
+            default: //Invalid Case
                 printf("Invalid Choice! \n");
                 break;
+        }
+            
     } while (choice != 0)
     
 }
@@ -80,6 +83,7 @@ void customerMenu(StockVariables *stock, StockVariables *sales)
 
     do
     {
+        //Menu For CUstomer
         printf("\n=====Customer Menu=====\n");
         printf("1.Buy Items\n");
         printf("2.View Inventory\n");
@@ -87,24 +91,26 @@ void customerMenu(StockVariables *stock, StockVariables *sales)
 
         choice = getInt("Enter choice: ", 0, 9);
 
-
+        //Switch Cases For All Cases
         switch(choice)
-        
-            case 1:
+        {
+             case 1: //Record The Sale Of CUstomer
                 recordSale(stock,sales);
                 break;
             
-            case 2:
+            case 2: //Display Inventory To Customer
                 displayStock(stock);
                 break;
 
-            case 0:
+            case 0: //Exiting The Program
                 printf("Logging Out...\n");
                 break;
 
-            default:
+            default: //Invalid Program
                 printf("Invalid Choice! \n");
                 break;
+        }
+
     } while (choice != 0)
 }
 
@@ -113,6 +119,7 @@ void mainMenu(StockVariables *stock , SaleVariables *sales)
     int choice;
     do
     {
+        //Main Menu
         printf("\n=========SuperMarket Management System==========\n");
         printf("1.Admin Mode\n");
         printf("2.Customer Mode \n");
@@ -120,24 +127,25 @@ void mainMenu(StockVariables *stock , SaleVariables *sales)
         choice = getInt("Enter choice: ", 0, 9);
 
 
+        //Switch Cases To get All Cases
         switch(choice)
         {
-            case 1:
+            case 1: //To Show Menu For Admin
                 adminMenu(&stock, &sales);
                 break;
 
-            case 2:
+            case 2: //To Show Menu For CUstomer
                 customerMenu(&stock, &sales);
                 break;
 
-            case 0:
+            case 0: //To Exit The Program after saving Data to FIle
                 saveSales(&sales);
                 saveStock(&stock);
 
                 printf("Data Saved. Exiting The Program...\n");
                 break;
 
-            default:
+            default: //Invalid Case
                 printf("Invalid Choice! \n");
                 break;
         }
