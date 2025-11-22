af736b112a9614a86182a61886e3a8d077b730cd
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-22 20:27:51 +0500

    Added COmments to main.c

--- source/main.c
+++ source/main.c
@@ -6,14 +6,18 @@
 
 int main()
 {
+    //Initializing Every Variable to 0
     StockVariables stock ={NULL, 0,0};
     SaleVariables sales ={NULL, 0,0};
 
+    //Loading The Stock and SALES if any
     loadStock(&stock);
     loadSales(&sales);
 
+    //Displaying The Main Menu
     mainMenu(&stock, &sales);
 
+    //Cleaning The Stock and Sales From The Memory At the End
     cleanStock(&stock);
     cleanSales(&sales);
 
