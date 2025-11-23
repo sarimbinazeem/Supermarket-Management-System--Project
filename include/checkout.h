809b5f096239f1c72688ea946cc77cf8cfcb2f20
Sarim Bin Azeem <sarimbinaz@gmail.com>
2025-11-24 00:09:53 +0500

    Removing Merging Conflicts: Header Files

--- include/checkout.h
+++ include/checkout.h
@@ -1,26 +1,9 @@
 #ifndef CHECKOUT_H
 #define CHECKOUT_H
 
-// Product structure for inventory
-struct Product {
-    int id;
-    char name[50];
-    float price;
-    int quantity;
-};
+#include "stock.h"
+#include "sales.h"
 
-// Cart item for billing
-struct CartItem {
-    int productId;
-    int quantity;
-    float price;
-    char name[50];
-};
-
-// Functions related to checkout process
-int loadInventory(struct Product items[], int maxItems);
-void saveInventory(struct Product items[], int *count);
-int findProductbyID(struct Product items[], int count, int id);
-void checkout();
+void checkout(StockVariables *stock, SaleVariables *sales);
 
 #endif
--- include/customer.h
+++ include/customer.h
@@ -1,17 +1,15 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
-
 struct Customer {
     char id[10];
     float totalSpent;
     int isVIP;
 };
 
-
-int loadCustomer(char *custID, struct Customer *cust);
+int loadCustomer(const char *custID, struct Customer *cust);
 void updateCustomer(struct Customer *cust);
-int addSpending(char *custID, float amount);
+int addSpending(const char *custID, float amount);
 void add_customer();
 void search_customer();
 
